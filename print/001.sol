6fe918c507b68fa594f4aff740786a276323b72c
Shohei Kobayashi <kobayashi_shohei@srockstyle.com>
2020-06-27 05:30:26 +0900

    README更新

---
+++ print/001.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.6.10;
+// SPDX-License-Identifier: MIT
+// 最初の一歩！
+contract HelloWorld {
+  function helloWorld() external pure returns (string memory) {
+    // returnでいいっぽい」」
+    return "Hello, World!";
+  }
+}
