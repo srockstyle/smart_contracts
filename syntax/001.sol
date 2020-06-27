d5f7c6aedb6295e270eba50af1c50c01fc8a58fe
Shohei Kobayashi <kobayashi_shohei@srockstyle.com>
2020-06-27 23:21:51 +0900

    コンパイルとった

--- syntax/001.sol
+++ syntax/001.sol
@@ -1,9 +1,8 @@
 pragma solidity ^0.6.10;
 // SPDX-License-Identifier: MIT
 
-// 最初の一歩！
-contract HelloWorld {
-  function helloWorld() external pure returns (string memory) {
-    return "Hello, World!";
-  }
+contract HelloWorld{
+    function Hello()external pure returns (string memory) {
+        return "Hello World";
+    }
 }
